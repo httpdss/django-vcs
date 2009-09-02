bb2bf50b678c928845deeb884104c2bfa655bbff
Kenneth <kenny@belitzky.com>
2009-09-02 14:11:40 -0300

    formated the views code and commented views

--- django_vcs/views.py
+++ django_vcs/views.py
@@ -7,22 +7,43 @@ from django.template import RequestContext
 from django_vcs.models import CodeRepository
 
 def repo_list(request):
+    """List of repositories 
+    
+    List of repositories taking into account that its has to be group-aware
+    
+    """
     repos = CodeRepository.objects.all()
-    return render_to_response('django_vcs/repo_list.html', {'repos': repos}, context_instance=RequestContext(request))
+    return render_to_response('django_vcs/repo_list.html', 
+                              {'repos': repos}, 
+                              context_instance = RequestContext(request))
 
 def recent_commits(request, slug):
-    repo = get_object_or_404(CodeRepository, slug=slug)
+    """List of recent commits from a given repository
+    
+    Keyword Arguments:
+    slug -- the repository slug
+    
+    """
+    repo = get_object_or_404(CodeRepository, slug = slug)
     commits = repo.get_recent_commits()
     return render_to_response([
         'django_vcs/%s/recent_commits.html' % repo.name,
         'django_vcs/recent_commits.html',
-    ], {'repo': repo, 'commits': commits}, context_instance=RequestContext(request))
+    ], {'repo': repo, 'commits': commits}, context_instance = RequestContext(request))
 
 def code_browser(request, slug, path):
-    repo = get_object_or_404(CodeRepository, slug=slug)
+    """Source code browser for a given repository
+        
+    Keyword Arguments:
+    slug -- the repository slug
+    path -- path inside the repository
+        
+    """
+    repo = get_object_or_404(CodeRepository, slug = slug)
     rev = request.GET.get('rev') or None
     context = {'repo': repo, 'path': path}
     file_contents = repo.get_file_contents(path, rev)
+    
     if file_contents is None:
         folder_contents = repo.get_folder_contents(path, rev)
         if folder_contents is None:
@@ -33,19 +54,25 @@ def code_browser(request, slug, path):
         return render_to_response([
             'django_vcs/%s/folder_contents.html' % repo.name,
             'django_vcs/folder_contents.html',
-        ], context, context_instance=RequestContext(request))
+        ], context, context_instance = RequestContext(request))
     context['file'] = file_contents
     return render_to_response([
         'django_vcs/%s/file_contents.html' % repo.name,
         'django_vcs/file_contents.html',
-    ], context, context_instance=RequestContext(request))
+    ], context, context_instance = RequestContext(request))
 
 def commit_detail(request, slug, commit_id):
-    repo = get_object_or_404(CodeRepository, slug=slug)
+    """Get details for a given commit
+        
+    slug -- the repository slug
+    commit_id -- identifier of the commit
+    
+    """
+    repo = get_object_or_404(CodeRepository, slug = slug)
     commit = repo.get_commit(commit_id)
     if commit is None:
         raise Http404
     return render_to_response([
         'django_vcs/%s/commit_detail.html' % repo.name,
         'django_vcs/commit_detail.html',
-    ], {'repo': repo, 'commit': commit}, context_instance=RequestContext(request))
+    ], {'repo': repo, 'commit': commit}, context_instance = RequestContext(request))
